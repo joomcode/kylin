0644ef57c0ffa9175b0dc44e2a729090f504729e
Alexander Sterligov <sterligovak@joom.it>
2017-08-06 22:06:55 +0300

    noticket compilation fixed

--- storage-hbase/src/main/java/org/apache/kylin/storage/hbase/cube/v2/CubeHBaseScanRPC.java
+++ storage-hbase/src/main/java/org/apache/kylin/storage/hbase/cube/v2/CubeHBaseScanRPC.java
@@ -105,7 +105,7 @@ public class CubeHBaseScanRPC extends CubeHBaseRPC {
             return Lists.newArrayList(halfCookedKey);//not shard to append at head, so it is already well cooked
         } else {
             List<byte[]> ret = Lists.newArrayList();
-            for (short i1 = 0; i < cuboidShardNum; ++i) {
+            for (short i = 0; i < cuboidShardNum; ++i) {
                 short shard = ShardingHash.normalize(cubeSeg.getCuboidBaseShard(cuboid.getId()), i, cubeSeg.getTotalShards(cuboid.getId()));
                 byte[] cookedKey = Arrays.copyOf(halfCookedKey, halfCookedKey.length);
                 BytesUtil.writeShort(shard, cookedKey, 0, RowConstants.ROWKEY_SHARDID_LEN);
